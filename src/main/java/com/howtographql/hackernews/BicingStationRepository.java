a86c7b6af44fdd272e96e9f9cf255d3aab0f6c07
Robert Almar <robert.almar@est.fib.upc.edu>
2017-10-26 11:34:20 +0200

    Queries en MetroAnsBusRepository hechas

--- src/main/java/com/howtographql/hackernews/BicingStationRepository.java
+++ src/main/java/com/howtographql/hackernews/BicingStationRepository.java
@@ -53,50 +53,9 @@ public class BicingStationRepository {
 					+ "<" + subject.toString() + "> <http://www.example.com/locatedIn> ?o." //GeographicalCoordinate
 					+ "?o <http://www.example.com/longitude> ?longitude."//GeographicalCoordinate
 					+ "?o <http://www.example.com/latitude> ?latitude." //GeographicalCoordinate
-					/*
-					+ "OPTIONAL {"
-							+ "<" + subject.toString() + "> <http://www.example.com/nearByInfrastructure> ?nearByInfrastructure."
-							+ "?nearByInfrastructure <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?tipoInfrastructure."
-							//+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stationID> ?nearByInfrastructureStationID.}"
-							//+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stationType> ?nearByInfrastructureStationType.}"
-							//+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stationStreetName> ?nearByInfrastructureStationStreetName.}"
-							//+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stationStatus> ?nearByInfrastructureStationStatus.}"
-							//+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stationStreetNumber> ?nearByInfrastructureStationStreetNumber.}"
-							//+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stationSlotsNumber> ?nearByInfrastructureSlotsNumber.}"
-							//+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stationBikesNumber> ?nearByInfrastructureStationBikesNumber.}"
-							//+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stationAltitude> ?nearByInfrastructureStationAltitude.}"
-							//+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/locatedIn> ?nearByInfrastructureLocatedIn.}"
-							//+ "OPTIONAL { ?nearByInfrastructureLocatedIn <http://www.example.com/longitude> ?nearByInfrastructureLongitude.}"
-							//+ "OPTIONAL { ?nearByInfrastructureLocatedIn <http://www.example.com/latitude> ?nearByInfrastructureLatitude.}"
-							
-							+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stopName> ?nearByInfrastructureStopName}." //metrobus
-							+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stopAddress> ?nearByInfrastructureStopAddress}." //metrobus
-							+ "OPTIONAL { ?nearByInfrastructure <http://www.example.com/stopPhone> ?nearByInfrastructureStopPhone}."
-							
-					+ "}."
-					*/	
-					
-					/*
-					+ "OPTIONAL {"
-							+ "<" + subject.toString() + "> <http://www.example.com/nearByStation> ?nearByStation."
-							+ "?nearByStation <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?nearByStationTipoInfrastructure."
-							+ "?nearByStation <http://www.example.com/stationID> ?nearByStationStationID."
-							+ "?nearByStation <http://www.example.com/stationType> ?nearByStationStationType."
-							+ "?nearByStation <http://www.example.com/stationStreetName> ?nearByStationStationStreetName."
-							+ "?nearByStation <http://www.example.com/stationStatus> ?nearByStationStationStatus."
-							+ "?nearByStation <http://www.example.com/stationStreetNumber> ?nearByStationStationStreetNumber."
-							+ "?nearByStation <http://www.example.com/stationSlotsNumber> ?nearByStationStationSlotsNumber."
-							+ "?nearByStation <http://www.example.com/stationBikesNumber> ?nearByStationStationBikesNumber."
-							+ "?nearByStation <http://www.example.com/stationAltitude> ?nearByStationStationAltitude."
-							+ "?nearByStation <http://www.example.com/locatedIn> ?nearByStationLocatedIn." //GeographicalCoordinate
-							+ "?nearByStationLocatedIn <http://www.example.com/longitude> ?nearByStationLongitude."//GeographicalCoordinate
-							+ "?nearByStationLocatedIn <http://www.example.com/latitude> ?nearByStationLatitude." //GeographicalCoordinate
-					+ "}."
-					*/
 					
 				+ "}");
 			
-			System.out.println(subject.toString());
 			
 			vqe = VirtuosoQueryExecutionFactory.create (sparql, graph);
 			ResultSet res2 = vqe.execSelect();
@@ -183,7 +142,6 @@ public class BicingStationRepository {
 			
 			while(res2.hasNext()){
 				QuerySolution qs2 = res2.next();
-				System.out.println(qs2);
 				
 				if(valid(qs2, "longitude")) longitude = Float.parseFloat(modifyScalarValue(qs2.get("longitude").toString()));
 				if(valid(qs2, "latitude")) latitude = Float.parseFloat(modifyScalarValue(qs2.get("latitude").toString()));	
@@ -196,55 +154,6 @@ public class BicingStationRepository {
 				if(valid(qs2, "stationStreetNumber")) stationStreetNumber = Integer.parseInt(modifyScalarValue(qs2.get("stationStreetNumber").toString()));
 				if(valid(qs2, "stationStatus")) stationStatus = modifyScalarValue(qs2.get("stationStatus").toString());
 
-				/*
-				if(valid(qs2, "nearByInfrastructure")){
-					if(valid(qs2, "tipoInfrastructure")){
-						if(qs2.get("tipoInfrastructure").toString().contains("BicingStation")){
-							
-							if(valid(qs2, "nearByInfrastructureStationStreetName")) nearStationStreetName =  modifyScalarValue(qs2.get("nearByInfrastructureStationStreetName").toString());
-							if(valid(qs2, "nearByInfrastructureStationType")) nearStationType = modifyScalarValue(qs2.get("nearByInfrastructureStationType").toString());
-							if(valid(qs2, "nearByInfrastructureStationBikesNumber")) nearStationBikesNumber = Integer.parseInt(modifyScalarValue(qs2.get("nearByInfrastructureStationBikesNumber").toString()));
-							if(valid(qs2, "nearByInfrastructureStationID")) nearStationID = Integer.parseInt(modifyScalarValue(qs2.get("nearByInfrastructureStationID").toString()));
-							if(valid(qs2, "nearByInfrastructureStationAltitude")) nearStationAltitude = Float.parseFloat(modifyScalarValue(qs2.get("nearByInfrastructureStationAltitude").toString()));
-							if(valid(qs2, "nearByInfrastructureSlotsNumber")) nearStationSlotsNumber = Integer.parseInt(modifyScalarValue(qs2.get("nearByInfrastructureSlotsNumber").toString()));
-							if(valid(qs2, "nearByInfrastructureStationStreetNumber")) nearStationStreetNumber = Integer.parseInt(modifyScalarValue(qs2.get("nearByInfrastructureStationStreetNumber").toString()));
-							if(valid(qs2, "nearByInfrastructureStationStatus")) nearStationStatus = modifyScalarValue(qs2.get("nearByInfrastructureStationStatus").toString());
-							if(valid(qs2, "nearByInfrastructureLatitude")) nearLatitude = Float.parseFloat(modifyScalarValue(qs2.get("nearByInfrastructureLatitude").toString()));
-							if(valid(qs2, "nearByInfrastructureLongitude")) nearLongitude = Float.parseFloat(modifyScalarValue(qs2.get("nearByInfrastructureLongitude").toString()));
-	
-							nearByInfrastructure.add(new BicingStation(null, new GeographicalCoordinate(nearLongitude, nearLatitude), "BicingStation", nearStationStreetName,nearStationType,nearStationBikesNumber, nearStationID, nearStationAltitude, nearStationSlotsNumber, nearStationStreetNumber, null,nearStationStatus ));
-							
-						}else if(qs2.get("tipoInfrastructure").toString().contains("MetroAndBusStop")){
-							if(valid(qs2, "nearByInfrastructureStopName")) nearStopName =  modifyScalarValue(qs2.get("nearByInfrastructureStopName").toString());
-							if(valid(qs2, "nearByInfrastructureStopAddress")) nearStopAddress = modifyScalarValue(qs2.get("nearByInfrastructureStopAddress").toString());
-							if(valid(qs2, "nearByInfrastructureStopPhone")) nearStopPhone = Integer.parseInt(modifyScalarValue(qs2.get("nearByInfrastructureStopPhone").toString()));
-							if(valid(qs2, "nearByInfrastructureLatitude")) nearLatitude = Float.parseFloat(modifyScalarValue(qs2.get("nearByInfrastructureLatitude").toString()));
-							if(valid(qs2, "nearByInfrastructureLongitude")) nearLongitude = Float.parseFloat(modifyScalarValue(qs2.get("nearByInfrastructureLongitude").toString()));
-							
-							nearByInfrastructure.add(new MetroAndBusStop(null, new GeographicalCoordinate(nearLongitude, nearLatitude), "MetroAndBusStop" , nearStopAddress,  nearStopPhone, nearStopName));
-							
-						}
-					}
-					
-				}
-				*/
-				/*
-				if(valid(qs2, "nearByStation")){
-					if(valid(qs2, "nearByStationStationStreetName")) nearStationStreetName =  modifyScalarValue(qs2.get("nearByStationStationStreetName").toString());
-					if(valid(qs2, "nearByStationStationType")) nearStationType = modifyScalarValue(qs2.get("nearByStationStationType").toString());
-					if(valid(qs2, "nearByStationStationBikesNumber")) nearStationBikesNumber = Integer.parseInt(modifyScalarValue(qs2.get("nearByStationStationBikesNumber").toString()));
-					if(valid(qs2, "nearByStationStationID")) {System.out.println("iep " + qs2.get("nearByStationStationID").toString() );nearStationID = Integer.parseInt(modifyScalarValue(qs2.get("nearByStationStationID").toString()));}
-					if(valid(qs2, "nearByStationStationAltitude")) nearStationAltitude = Float.parseFloat(modifyScalarValue(qs2.get("nearByStationStationAltitude").toString()));
-					if(valid(qs2, "nearByStationSlotsNumber")) nearStationSlotsNumber = Integer.parseInt(modifyScalarValue(qs2.get("nearByStationSlotsNumber").toString()));
-					if(valid(qs2, "nearByStationStationStreetNumber")) nearStationStreetNumber = Integer.parseInt(modifyScalarValue(qs2.get("nearByStationStationStreetNumber").toString()));
-					if(valid(qs2, "nearByStationStationStatus")) nearStationStatus = modifyScalarValue(qs2.get("nearByStationStationStatus").toString());
-					if(valid(qs2, "nearByStationLatitude")) nearLatitude = Float.parseFloat(modifyScalarValue(qs2.get("nearByStationLatitude").toString()));
-					if(valid(qs2, "nearByStationLongitude")) nearLongitude = Float.parseFloat(modifyScalarValue(qs2.get("nearByStationLongitude").toString()));
-					
-					nearBicingStation.add(new BicingStation(null, new GeographicalCoordinate(nearLongitude, nearLatitude), "BicingStation", nearStationStreetName,nearStationType,nearStationBikesNumber, nearStationID, nearStationAltitude, nearStationSlotsNumber, nearStationStreetNumber, null,nearStationStatus ));
-				
-				}
-				*/
 				while(res3.hasNext()){
 					qs2 = res3.next();
 					if(valid(qs2, "nearByInfrastructure")){
